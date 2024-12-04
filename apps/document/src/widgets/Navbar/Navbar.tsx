0dccf6c793df4698295020153285e64666941936
uvarovag <aleanches@gmail.com>
2024-12-04 23:15:03 +0300

    feature: external navigation

--- apps/document/src/widgets/Navbar/Navbar.tsx
+++ apps/document/src/widgets/Navbar/Navbar.tsx
@@ -3,6 +3,9 @@ import { Link } from 'react-router'
 export const Navbar = () => (
     <ul>
         <li>
+            <Link to="/user">to user app</Link>
+        </li>
+        <li>
             <Link to="./">main</Link>
         </li>
     </ul>
--- apps/user/src/widgets/Navbar/Navbar.tsx
+++ apps/user/src/widgets/Navbar/Navbar.tsx
@@ -3,6 +3,9 @@ import { Link } from 'react-router'
 export const Navbar = () => (
     <ul>
         <li>
+            <Link to="/document">to document app</Link>
+        </li>
+        <li>
             <Link to="./">main</Link>
         </li>
     </ul>
